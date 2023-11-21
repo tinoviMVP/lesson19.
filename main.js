9a5e9146228b5e31c66f47c94224026e57f081b8
tinoviMVP <sereda200524@gmail.com>
2023-11-21 21:11:25 +0500

    fs

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const inputField = document.getElementById('inputField');
 const keyboard = document.getElementById('keyboard');
+const shiftButton = createButton('Shift');
 
 const keys = [
 ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'],
@@ -9,37 +10,62 @@ const keys = [
 ];
 
 keys.forEach(row => {
-const rowDiv = document.createElement('div');
-rowDiv.className = 'row';
+const rowDiv = createDiv('row');
 
 row.forEach(key => {
-const button = document.createElement('button');
-button.className = 'button';
-button.textContent = key;
+const button = createButton(key);
 button.addEventListener('click', () => {
 inputField.value += key;
 });
-
 rowDiv.appendChild(button);
 });
 
 keyboard.appendChild(rowDiv);
 });
 
-document.addEventListener('keydown', e => {
+document.addEventListener('keydown', handleKeyDown);
+document.addEventListener('keyup', handleKeyUp);
+
+const backspaceButton = createButton('Backspace');
+backspaceButton.addEventListener('click', () => {
+inputField.value = inputField.value.slice(0, -1);
+});
+keyboard.appendChild(backspaceButton);
+document.addEventListener('keydown', handleBackspaceKeyDown);
+document.addEventListener('keyup', handleBackspaceKeyUp);
+
+shiftButton.addEventListener('click', handleShiftButtonClick);
+keyboard.appendChild(shiftButton);
+document.addEventListener('keydown', handleShiftKeyDown);
+document.addEventListener('keyup', handleShiftKeyUp);
+
+function createButton(text) {
+const button = document.createElement('button');
+button.className = 'button';
+button.textContent = text;
+return button;
+}
+
+function createDiv(className) {
+const div = document.createElement('div');
+div.className = className;
+return div;
+}
+
+function handleKeyDown(e) {
 const key = e.key;
 
 if (/[a-zA-Z0-9]/.test(key)) {
 inputField.value += key;
 highlightKey(key);
 } else if (key === 'Backspace') {
-e.preventDefault(); 
+e.preventDefault();
 inputField.value = inputField.value.slice(0, -1);
 highlightBackspaceButton();
 }
-});
+}
 
-document.addEventListener('keyup', e => {
+function handleKeyUp(e) {
 const key = e.key;
 
 if (/[a-zA-Z0-9]/.test(key)) {
@@ -47,7 +73,7 @@ removeHighlightKey(key);
 } else if (key === 'Backspace') {
 removeHighlightBackspaceButton();
 }
-});
+}
 
 function highlightKey(key) {
 const buttons = document.getElementsByClassName('button');
@@ -71,32 +97,23 @@ break;
 }
 }
 
-const backspaceButton = document.createElement('button');
-backspaceButton.className = 'button';
-backspaceButton.textContent = 'Backspace';
-
-backspaceButton.addEventListener('click', () => {
-inputField.value = inputField.value.slice(0, -1);
-});
-
-keyboard.appendChild(backspaceButton);
-
-document.addEventListener('keydown', e => {
+function handleBackspaceKeyDown(e) {
 const key = e.key;
 
 if (key === 'Backspace') {
-e.preventDefault(); // Prevent browser back navigation
+e.preventDefault();
 highlightBackspaceButton();
+inputField.value = inputField.value.slice(0, -1);
 }
-});
+}
 
-document.addEventListener('keyup', e => {
+function handleBackspaceKeyUp(e) {
 const key = e.key;
 
 if (key === 'Backspace') {
 removeHighlightBackspaceButton();
 }
-});
+}
 
 function highlightBackspaceButton() {
 backspaceButton.classList.add('active');
@@ -105,3 +122,36 @@ backspaceButton.classList.add('active');
 function removeHighlightBackspaceButton() {
 backspaceButton.classList.remove('active');
 }
+
+function handleShiftButtonClick() {
+
+}
+
+function handleShiftKeyDown(e) {
+const key = e.key;
+
+if (key === 'Shift') {
+highlightShiftButton();
+}
+}
+
+function handleShiftKeyUp(e) {
+const key = e.key;
+
+if (key === 'Shift') {
+removeHighlightShiftButton();
+}
+}
+
+function highlightShiftButton() {
+shiftButton.classList.add('active');
+}
+
+function removeHighlightShiftButton() {
+shiftButton.classList.remove('active');
+}
+
+
+
+
+
